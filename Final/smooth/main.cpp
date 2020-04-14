3f4400954417e5b6e3fa4b99caac55f5875346d5
HViktorTsoi <cuijiahetony@live.com>
2020-04-14 13:41:45 +0800

    Add cmake and main for smooth

---
+++ Final/smooth/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by hviktortsoi on 20-4-12.
+//
+#include <stdio.h>
+#include <stdlib.h>
+
+void smooth_wrapper();
+
+int main(int argc, char **argv) {
+    smooth_wrapper();
+    return 0;
+}
